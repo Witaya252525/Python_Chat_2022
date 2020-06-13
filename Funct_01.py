547dee8c923e1e9d73211b91deebaa92f4db6fd8
Witaya252525 <60280369+Witaya252525@users.noreply.github.com>
2020-06-14 00:40:42 +0700

    function and return
    one arguement

--- Funct_01.py
+++ Funct_01.py
@@ -1,30 +1,22 @@
 
-'''
-def get_area (pi ,radius):
-    area = pi*(radius**2)
-    return area , pi
-
-
-print(get_area(pi =3.14 ,radius=22)) 
-
-'''
 def get_area (radius):
     radius+=1
     pi = 3.14
-    print(radius)
     print("in_function")
-    print("radius = %0.1d" %(radius))
-    AA = (pi*(radius*radius))
-    print(pi)
-    return  AA
+    print("radius = %d" %(radius))
+    my_area = (pi*(radius*radius))
+    return  (print("The are is" + "  "  + str(my_area)))
 
-# pi =3.14
-radius = 20
+
+
 get_area(50)
+radius = 20
+pi = 3.4505
 print("after function")  
+print(" >>>>>>> ")
 print(radius) 
 print(pi)
-print(" >>>>>>> ")
 print(get_area(radius))
+
 
 
