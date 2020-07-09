3077f78989ae4f2bbf2f79e8b9484aa8b4d9a9ff
Witaya252525 <60280369+Witaya252525@users.noreply.github.com>
2020-07-10 05:27:19 +0700

    Update string.py
    cut one string

--- July_2020/string.py
+++ July_2020/string.py
@@ -6,7 +6,14 @@
 # print("This is %s and %d"  %(a,b))
 
 
-s = "Hello World"
-for i in range(len(s)):
-    if s[i] =="W":
-        print(s[i:])
+# s = "Hello World"
+# for i in range(len(s)):
+#     if s[i] =="W":
+#         print(s[i:])
+
+
+
+text = "Today is Sunday. I stay at home and keep coding. "
+word ="keep"
+next_word = text[text.find(word )+ len(word)+1 : ]
+print(next_word)
