7af8c8afcda23049082e49e8ac4bb1d817a66c7e
Witaya252525 <60280369+Witaya252525@users.noreply.github.com>
2020-10-18 17:11:29 +0700

    oct 19

---
+++ Oct_2020/area.py
@@ -0,0 +1,16 @@
+
+import math
+
+
+def rectangle(w,h):
+    return w*h
+
+
+def circle(r):
+    return (3.14*r*r)
+
+
+
+if __name__ == "__main__":
+    print(rectangle(5,10))
+
--- list_2.py
+++
@@ -1,6 +0,0 @@
-
-
-name_list = [['Supasate' ,'Choochaisri'] , ["Somchai" ,"Python"]]
-print(name_list)
-print(name_list[0])
-print(name_list[0][0])
