74442f0e31f3c608277e35df25fb7ee573ed120a
Witaya252525 <witaya.c@kkumail.com>
2020-10-18 22:58:03 +0700

    add list

--- July_2020/Funct_01.py
+++ July_2020/Funct_01.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 def get_area (radius):
     radius+=1
@@ -19,29 +18,3 @@ print(radius)
 print(pi)
 print(get_area(radius))
 
-
-
-=======
-
-def get_area (radius):
-    radius+=1
-    pi = 3.14
-    print("in_function")
-    print("radius = %d" %(radius))
-    my_area = (pi*(radius*radius))
-    return  (print("The are is" + "  "  + str(my_area)))
-
-
-
-get_area(50)
-radius = 20
-pi = 3.4505
-print("after function")  
-print(" >>>>>>> ")
-print(radius) 
-print(pi)
-print(get_area(radius))
-
-
-
->>>>>>> ed0daa58b012cca843f4066776d2bfe28585b88e
---
+++ Oct_2020/list.py
@@ -0,0 +1,17 @@
+
+weekdays = ['monday' , 'tuesday' , 'wensday' , 'thusday' ,'friday']
+print(weekdays)
+print(len(weekdays))
+for day in weekdays:
+    print(" to day is " ,  day)
+
+weekdays[2] = "wiatya"
+print(weekdays)
+
+
+
+
+
+
+
+    
