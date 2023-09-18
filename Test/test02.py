ba16298d4247784d2b20e4e8d40b3d3e20e6fb22
Witaya252525 <witaya.c@kkumail.com>
2023-09-19 05:49:21 +0700

    add funtion  find more words

--- Test/test02.py
+++ Test/test02.py
@@ -8,17 +8,32 @@
 
 # get_triangle_area (20,20)
 
-def get_area (pi,radius):
-    print("in function")
-    print(" pi = %0.4f ,radius = %d" %(pi,radius))
-    return pi*(radius**2)
+# def get_area (pi,radius):
+#     print("in function")
+#     print(" pi = %0.4f ,radius = %d" %(pi,radius))
+#     return pi*(radius**2)
 
-pi=3.1433
-radius =3
-area = get_area (3.1416,5)
-print("after  function")
-print(" pi = %0.4f ,radius = %d" %(pi,radius))
+# pi=3.1433
+# radius =3
+# area = get_area (3.1416,5)
+# print("after  function")
+# print(" pi = %0.4f ,radius = %d" %(pi,radius))
+
+def find_next_word (text,word,start_pos):
+    text = text[text.find(word,start_pos) +len(word)+1  : ]
+    next_word = text[  :text.find(" ")]
+    print(next_word)
+    start_pos = text.find(" ") +1
+
+    return text ,start_pos
 
 
 
 
+
+start_pos = 0
+text = "Today is Sunday i stay at home and keep coding Today is Monday  I stay at office and keep cycling but still keep playing."
+word = "keep"
+text ,start_pos = find_next_word(text,word,start_pos)
+text ,start_pos = find_next_word(text,word,start_pos)
+text ,start_pos = find_next_word(text,word,start_pos)
