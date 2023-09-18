c52b6a9a2a907548498198517656ff3b43c2f29e
Witaya252525 <witaya.c@kkumail.com>
2023-09-18 21:30:43 +0700

    after function

---
+++ Test/test02.py
@@ -0,0 +1,24 @@
+
+
+
+# def get_triangle_area(height,base_width):
+#     area = ( 0.5*height *base_width )
+#     return print (area)
+
+
+# get_triangle_area (20,20)
+
+def get_area (pi,radius):
+    print("in function")
+    print(" pi = %0.4f ,radius = %d" %(pi,radius))
+    return pi*(radius**2)
+
+pi=3.1433
+radius =3
+area = get_area (3.1416,5)
+print("after  function")
+print(" pi = %0.4f ,radius = %d" %(pi,radius))
+
+
+
+
