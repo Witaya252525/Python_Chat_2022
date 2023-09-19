b778da1bf20d8b994ee4ff88a4d723be36da9931
Witaya252525 <witaya.c@kkumail.com>
2023-09-19 18:51:40 +0700

    Add loop

--- Test_Program/test03.py
+++ Test_Program/test03.py
@@ -1,35 +1,13 @@
 
-
-# def get_triangle_area(height,base_width):
-#     area = ( 0.5*height *base_width )
-#     return print (area)
-
-
-# get_triangle_area (20,20)
-
-# def get_area (pi,radius):
-#     print("in function")
-#     print(" pi = %0.4f ,radius = %d" %(pi,radius))
-#     return pi*(radius**2)
-
-# pi=3.1433
-# radius =3
-# area = get_area (3.1416,5)
-# print("after  function")
-# print(" pi = %0.4f ,radius = %d" %(pi,radius))
-
-def find_next_word(text, word, start_pos):
-    # text = text[text.find(word,start_pos) +len(word)+1  : ]
-    # next_word = text[  :text.find(" ")]
-    # print(next_word)
-
+def find_next_word(text,word,start_pos):
+    
     if text.find(word, start_pos) != -1:
+
         text = text[text.find(word, start_pos) + len(word)+1:]
 
-
-        if text.find(" ") != -1:
+        if text.find(" ")!= -1:
             next_word = text[:text.find(" ")]
-            start_pos = text.find(" ") + 1
+            start_pos = text.find(" ")+ 1
             print(next_word)
 
         else:
@@ -40,23 +18,15 @@ def find_next_word(text, word, start_pos):
             
         return text ,start_pos
 
-
-
-
-
     else:
         print(" ")
-        # return "" , -1
-
-    return text, start_pos
+        return " ", -1
 
 
-start_pos = 0
-text = "Today is Sunday i stay at home and keep coding Today is Monday  I stay at office and keep cycling but still keep playing."
+text = "Today is Sunday i stay at home and keep coding Today is Monday  I stay at office and keep cycling but still keep playing i go to bangkok to keep motivation "
 word = "keep"
-# text, start_pos = find_next_word(text, word, start_pos)
-# text, start_pos = find_next_word(text, word, start_pos)
-# text, start_pos = find_next_word(text, word, start_pos)
-while text.find(" ") != -1 :
-    text, start_pos = find_next_word(text, word, start_pos)
+start_pos = 0
+while start_pos !=-1 :
+    text,start_pos = find_next_word(text,word,start_pos)
+    
 print("YOU are Awesome Man")
