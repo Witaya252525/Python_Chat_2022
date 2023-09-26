299d065717b2e8884958746ee2b9e3ba7d43c719
Witaya252525 <witaya.c@kkumail.com>
2023-09-27 06:08:29 +0700

    Sep_27

--- Test_Program/For_01.py
+++ Test_Program/For_01.py
@@ -1,3 +1,14 @@
-Weekdays = ['Monday', 'Tuesday' ,'Wenessday',"Thusday",'Friday']
-for days in Weekdays:
-    print(f' Today is {days}')
+# Weekdays = ['Monday', 'Tuesday' ,'Wenessday',"Thusday",'Friday']
+# for days in Weekdays:
+#     print(f' Today is {days}')
+
+
+
+
+# for i in range(2,10,2):
+#         print(i)
+
+
+for i in range (2,11):
+    if i%2 == 0:
+        print(i)                                              
